36457546457eee86678af20cc26867f4530011b6
david <tlaw4444@gmail.com>
2025-03-26 11:56:34 +0100

    Modified initial data and disabled swagger

--- src/main/java/org/openapitools/api/ObjectsApiController.java
+++ src/main/java/org/openapitools/api/ObjectsApiController.java
@@ -5,6 +5,7 @@ import org.openapitools.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
 
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@CrossOrigin("*")
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-03-24T18:55:33.945312570+01:00[Europe/Madrid]", comments = "Generator version: 7.12.0")
 @Controller
 @RequestMapping("${openapi.objectManagement.base-path:}")
@@ -22,7 +24,9 @@ public class ObjectsApiController implements ObjectsApi {
     private final ObjectMapper mapper = new ObjectMapper();
 
     static{
-        lista.add(new ModelObject().id(UUID.randomUUID().toString()).name("Peluche Tiburón").data(new ModelData().photo("test").description("ssdada").price(0.0)));
+        lista.add(new ModelObject().id(UUID.randomUUID().toString()).name("Peluche Tiburón").data(new ModelData().photo("https://www.ikea.com/es/es/images/products/blahaj-peluche-peluche-tiburon__0716615_pe730956_s5.jpg").description("Peluche de tiburón de 40x40cm").price(30.0)));
+        lista.add(new ModelObject().id(UUID.randomUUID().toString()).name("Figura Super Mario").data(new ModelData().photo("https://afede.com/18126-large_default/figura-super-mario-50cm-.jpg").description("Figura de Super Mario Bros, edición especial").price(50.0)));
+        lista.add(new ModelObject().id(UUID.randomUUID().toString()).name("Atari ET").data(new ModelData().photo("https://www.chollogames.es/67159-large_default/atari-et-h442.jpg").description("El mejor juego nunca hecho, aventura interactiva").price(999.99)));
     }
 
 
